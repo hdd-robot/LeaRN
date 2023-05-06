6872c4bc79d287441e3867c107d37d31b1dd0516
Halim Djerroud <hdd@halim.info>
2023-05-06 15:03:20 +0200

    class

---
+++ src/Class.h
@@ -0,0 +1,19 @@
+#ifndef _LEA_CLASS_H_
+#define _LEA_CLASS_H_
+
+#include "DataType.h"
+
+
+namespace LeaRN {
+  class Class : public DataType {
+  private:
+
+
+  public:
+    virtual ~Class(){}
+
+
+  };
+
+} // namespace LeaRN
+#endif
